29143ce71757be0f46cc1e3c73c9874f67c9c51f
eral.r.l <eral.r.l@gmail.com>
2014-01-13 03:58:32 +0900

    変換UI周りの調整

--- Editor/Inspector/VMDInspector.cs
+++ Editor/Inspector/VMDInspector.cs
@@ -4,37 +4,32 @@ using System.Collections;
 using MMD.PMD;
 using System.IO;
 
-#if !(UNITY_3_5 || UNITY_3_4 || UNITY_3_3)
-
 namespace MMD
 {
-	[CustomEditor(typeof(VMDScriptableObject))]
+    [CustomEditor(typeof(VMDScriptableObject))]
     public class VMDInspector : Editor
     {
         // VMD Load option
-        public bool createAnimationFile;
-        public int interpolationQuality;
         public GameObject pmdPrefab;
+        VMDImportConfig vmd_config;
 
         // last selected item
-        private static MotionAgent motion_agent;
-        private static string message = "";
+        private MotionAgent motion_agent;
+        private string message = "";
 
         /// <summary>
-        /// 選択されているオブジェクトがVMDファイルかチェックします
+        /// 有効化処理
         /// </summary>
-        /// <returns>VMDファイルであればそのパスを、異なればnullを返します。</returns>
-        void setup()
+        void OnEnable()
         {
             // デフォルトコンフィグ
             var config = MMD.Config.LoadAndCreate();
-            createAnimationFile = config.vmd_config.createAnimationFile;
-            interpolationQuality = config.vmd_config.interpolationQuality;
+            vmd_config = config.vmd_config.Clone();
 
             // モデル情報
             if (config.inspector_config.use_vmd_preload)
             {
-				var obj = (VMDScriptableObject)target;
+                var obj = (VMDScriptableObject)target;
                 motion_agent = new MotionAgent(obj.assetPath);
             }
             else
@@ -48,14 +43,11 @@ namespace MMD
         /// </summary>
         public override void OnInspectorGUI()
         {
-            setup();
-
             // GUIの有効化
-            GUI.enabled = true;
+            GUI.enabled = !EditorApplication.isPlaying;
 
-            pmdPrefab = EditorGUILayout.ObjectField("PMD Prefab", pmdPrefab, typeof(Object), false) as GameObject;
-            createAnimationFile = EditorGUILayout.Toggle("Create Asset", createAnimationFile);
-            interpolationQuality = EditorGUILayout.IntSlider("Interpolation Quality", interpolationQuality, 1, 10);
+            pmdPrefab = (GameObject)EditorGUILayout.ObjectField("PMD Prefab", pmdPrefab, typeof(Object), false);
+            vmd_config.OnGUIFunction();
 
             // Convertボタン
             EditorGUILayout.Space();
@@ -65,25 +57,28 @@ namespace MMD
             }
             else
             {
+                bool gui_enabled_old = GUI.enabled;
+                GUI.enabled = (null != pmdPrefab);
                 if (GUILayout.Button("Convert"))
                 {
                     if (null == motion_agent) {
-						var obj = (VMDScriptableObject)target;
+                        var obj = (VMDScriptableObject)target;
                         motion_agent = new MotionAgent(obj.assetPath);
                     }
-                    motion_agent.CreateAnimationClip(pmdPrefab, createAnimationFile, interpolationQuality);
+                    motion_agent.CreateAnimationClip(pmdPrefab
+                                                    , vmd_config.createAnimationFile
+                                                    , vmd_config.interpolationQuality
+                                                    );
                     message = "Loading done.";
                 }
+                GUI.enabled = gui_enabled_old;
             }
             GUILayout.Space(40);
 
             // モデル情報
             if (motion_agent == null) return;
             EditorGUILayout.LabelField("Model Name");
-            GUI.enabled = false;
-            EditorGUILayout.TextArea(motion_agent.model_name);
-            GUI.enabled = true;
+            EditorGUILayout.LabelField(motion_agent.model_name, EditorStyles.textField);
         }
     }
 }
-#endif
