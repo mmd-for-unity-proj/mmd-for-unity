d07d387b93d91f602b8f837860c315035cd30e98
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-02 04:03:35 +0900

    add vmd formatter

---
+++ Exporter/VMD/VMDFormatter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using MMD.VMD;
+
+class VMDFormatter
+{
+    GameObject mmd_object;
+
+    public VMDFormatter(GameObject target)
+    {
+        mmd_object = target;
+    }
+}
