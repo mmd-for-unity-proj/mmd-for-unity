22383c625eb4c303c6f6f888cf5b36606fd0b53f
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-02 04:03:49 +0900

    move directory

---
+++ Exporter/VMD/VMDExporter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using MMD.VMD;
+
+public class VMDExporter
+{
+    VMDFormat format;
+
+    public VMDExporter(VMDFormat format)
+    {
+        this.format = format;
+    }
+}
