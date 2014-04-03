0f63ad874893dc005ab311d262ddcc97ab51eab0
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-03 14:19:36 +0900

    coding vmd formatter

--- Exporter/VMD/VMDFormatter.cs
+++ Exporter/VMD/VMDFormatter.cs
@@ -1,13 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using MMD.VMD;
 
 class VMDFormatter
 {
     GameObject mmd_object;
+    VMDFormat format;
 
     public VMDFormatter(GameObject target)
     {
         mmd_object = target;
+        format = new VMDFormat();
+        format.motion_list = new VMDFormat.MotionList();
+        format.skin_list = new VMDFormat.SkinList();
+        format.camera_list = new VMDFormat.CameraList();
+        format.light_list = new VMDFormat.LightList();
+        format.self_shadow_list = new VMDFormat.SelfShadowList();
+    }
+
+    public VMDFormat InsertMorph(int insert_frame_no)
+    {
+        var expression = mmd_object.transform.FindChild("Expression");
+        var expressions = new List<Transform>();
+        for (int i = 0; i < expression.childCount; i++)
+            expressions.Add(expression.GetChild(i));
+
+        
+
+        return format;
     }
 }
