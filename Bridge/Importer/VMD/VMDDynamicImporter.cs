72d2a910907f5fd2bd4b5accd49eb2debed329ee
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-13 19:32:01 +0900

    add dynamic importer

---
+++ Bridge/Importer/VMD/VMDDynamicImporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MMD
+{
+    namespace VMD
+    {
+        public class VMDDynamicImporter
+        {
+            public static void Import(GameObject pmd_object, byte[] data)
+            {
+                var format = VMDFormatFactory.Import(data);
+                VMDConverter.CreateAnimationClip(format, pmd_object, 1);
+            }
+        }
+    }
+}
