4b2fa6d1469c6c73b32760c38495c9fca6e8350d
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-15 13:25:40 +0900

    rename file

---
+++ Bridge/Importer/VMD/VMDImporter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MMD
+{
+    namespace VMD
+    {
+        public class VMDImporter
+        {
+            /// <summary>
+            /// VMDファイル形式のバイナリデータをインポートする
+            /// </summary>
+            /// <param name="pmd_object">PMD/PMXから変換されたGameObject</param>
+            /// <param name="data">VMDファイルのバイナリ</param>
+            /// <param name="clip_name">AnimationClipに付けたいモーション名</param>
+            public static void Import(GameObject pmd_object, byte[] data, string clip_name)
+            {
+                var format = VMDFormatFactory.Import(data);
+                var clip = VMDConverter.CreateAnimationClip(format, pmd_object, 1);
+                var animation = pmd_object.GetComponent<Animation>();
+                if (animation != null)
+                    animation.AddClip(clip, clip_name);
+                else
+                    Debug.Log("Failed to import " + clip_name + " for " + pmd_object.name + ".");
+            }
+        }
+    }
+}
