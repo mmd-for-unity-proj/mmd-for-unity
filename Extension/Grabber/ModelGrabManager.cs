45074ca4943642a3bc5307b5b83b83f7f2084b42
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-19 00:49:48 +0900

    add model grab manager

---
+++ Extension/Grabber/ModelGrabManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class ModelGrabManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
