05d302ef64d1c745e6856ad6163a145aa972f4be
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-19 01:05:30 +0900

    add change grab mode

--- Extension/Grabber/ModelGrabManager.cs
+++ Extension/Grabber/ModelGrabManager.cs
@@ -6,13 +6,43 @@ using UnityEngine;
 
 public class ModelGrabManager : MonoBehaviour
 {
+    GameObject grab;
+
+    bool is_ik;
+
+    enum Mode
+    {
+        Rotate,
+        Translate
+    }
+    Mode mode = Mode.Rotate;
+
     void Start()
     {
-
+        grab = new GameObject("GrabCircle");
+        grab.transform.position = new Vector3(-1f, -1f, 0);
+        grab.AddComponent<GrabCircle>();
     }
 
     void Update()
     {
+        ChangeGrabMode(mode);
+    }
 
+    void ChangeGrabMode(Mode mode)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (mode == Mode.Rotate)
+                mode = Mode.Translate;
+            else
+                mode = Mode.Rotate;
+        }
+    }
+
+    void ViewCircle(GameObject target_bone)
+    {
+        var screen_point = Camera.main.ViewportToScreenPoint(target_bone.transform.position);
+        grab.transform.position = screen_point;
     }
 }
