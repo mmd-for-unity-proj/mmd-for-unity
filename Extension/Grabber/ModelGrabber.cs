ca8e3531dfbd6adb8b0183e16a13cb27357d0150
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-19 00:33:08 +0900

    add initialize model grabber

--- Extension/Grabber/ModelGrabber.cs
+++ Extension/Grabber/ModelGrabber.cs
@@ -8,18 +8,16 @@ public class ModelGrabber : MonoBehaviour
 {
     void Start()
     {
-
+        const float size = 64f;
+        gameObject.transform.localScale = new Vector3(0, 0, 1);
+        gameObject.AddComponent<GUITexture>();
+        gameObject.guiTexture.texture = Resources.Load("white_circle") as Texture2D;
+        gameObject.guiTexture.color = new Color(1f, 0f, 0f, 0.25f);
+        gameObject.guiTexture.pixelInset = new Rect(-size * 0.5f, -size * 0.5f, size, size);
     }
 
     void Update()
     {
 
-    }
-
-    void CreateGrabObject()
-    {
-        var grab = new GameObject("grab");
-        grab.AddComponent<MeshRenderer>();
-        var shader = Shader.Find("");
     }
 }
