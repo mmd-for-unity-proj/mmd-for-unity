6527bffb450354d70d44482696b04289199f443b
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-18 23:18:33 +0900

    add model grabber script

---
+++ Extension/Grabber/ModelGrabber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class ModelGrabber : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void CreateGrabObject()
+    {
+        var grab = new GameObject("grab");
+        grab.AddComponent<MeshRenderer>();
+        var shader = Shader.Find("");
+    }
+}
