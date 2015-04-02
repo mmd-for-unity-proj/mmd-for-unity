0c98f4390904b5950b70cb3f8df9645e978156e7
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-04-02 16:52:17 +0900

    フォーマットの追加

---
+++ Format/PMD/Bone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MMD
+{
+    namespace Format
+    {
+        namespace PMD
+        {
+            public class VertexList
+            {
+                public uint VertexCount { get; set; }
+                public List<Vertex> Vertices { get; set; }
+            }
+
+            public class Vertex
+            {
+                public Vector3 Position { get; set; }
+                public Vector3 Normal { get; set; }
+                public Vector2 UV { get; set; }
+                
+            }
+        }
+    }
+}
---
+++ Format/PMD/Header.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MMD
+{
+    namespace Format
+    {
+        namespace PMD
+        {
+            public class Header
+            {
+                public string Magic { get; set; }
+                public float Version { get; set; }
+                public string ModelName { get; set; }
+                public string Comment { get; set; }
+            }
+        }
+    }
+}
