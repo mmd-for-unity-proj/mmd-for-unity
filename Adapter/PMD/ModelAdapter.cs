c8a64e02709feeae32cb8c291e322379041392ec
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-04-04 20:55:55 +0900

    BuilderとAdapter追加

---
+++ Adapter/PMD/ModelAdapter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using MMD.Format.Common;
+using MMD.Format.PMD;
+
+namespace MMD.Adapter.PMD
+{
+    public class ModelAdapter
+    {
+        public UnityEngine.Vector3[] Vertices(VertexList vertices)
+        {
+            var vectors = new UnityEngine.Vector3[vertices.Count];
+
+            for (int i = 0; i < vertices.Count; ++i)
+            {
+                vectors[i].x = vertices[i].position.x;
+                vectors[i].y = vertices[i].position.y;
+                vectors[i].z = vertices[i].position.z;
+            }
+
+            return vectors;
+        }
+
+        public UnityEngine.Vector3[] Nolmals(VertexList vertices)
+        {
+            var vectors = new UnityEngine.Vector3[vertices.Count];
+
+            for (int i = 0; i < vertices.Count; ++i)
+            {
+                vectors[i].x = vertices[i].normal.x;
+                vectors[i].y = vertices[i].normal.y;
+                vectors[i].z = vertices[i].normal.z;
+            }
+
+            return vectors;
+        }
+
+        public UnityEngine.Vector2[] UVs(VertexList vertices)
+        {
+            var vectors = new UnityEngine.Vector2[vertices.Count];
+
+            for (int i = 0; i < vertices.Count; ++i)
+            {
+                vectors[i].x = vertices[i].uv.x;
+                vectors[i].y = vertices[i].uv.y;
+            }
+
+            return vectors;
+        }
+    }
+}
---
+++ Builder/PMD/ModelBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MMD.Adapter;
+using UnityEngine;
+
+namespace MMD.Builder.PMD
+{
+    public class ModelBuilder
+    {
+        Mesh mesh = new Mesh();
+
+        void Read()
+        {
+            
+        }
+    }
+}
